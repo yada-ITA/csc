b557b7cf9c240f116b99036badfaa474b455720b
matu0813 <kouichi_matsumoto@yanmar.co.jp>
2013-09-05 10:05:33 +0900

    branc4最新

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+  attr_accessor :login
+
+  before_validation :set_email_and_userno_downcase :private
+  
+  def set_email_and_userno_downcase
+    #self.email = self.original_email.downcase
+    #self.userno = self.original_userno.downcase
+  end
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(userno) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
